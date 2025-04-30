cb646a0b3bdfdf3dcde26630ed86fc20df95cd9f
Noor <ansarinoorafshan14@gmail.com>
2025-04-30 20:56:23 +0530

    working on header vto make responsive

---
+++ src/page/Admin/Dashboard/Component/MyInvoice/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+
+const MyInvoice = ({ handleButtonClick }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "90%",
+        margin: "0 auto",
+        pt: 2,
+      }}
+    >
+      <Box
+        sx={{
+          width: "70%",
+          marginRight: "8px",
+          marginTop: "10px",
+        }}
+      >
+        <Typography variant="h6" fontWeight="bold">
+          My Invoice
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: "15%",
+          marginTop: "10px",
+        }}
+      >
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#009e74",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "#007a5c",
+            },
+          }}
+          onClick={handleButtonClick}
+        >
+          New Invoice
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default MyInvoice;
